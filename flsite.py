8ec137e1efb19de239e62ebeab3ccee6233f1bb5
Darkushaa <darkusha2.0@gmail.com>
2023-03-23 15:18:09 +0200

    Lesson 9

--- flsite.py
+++ flsite.py
@@ -1,62 +1,7 @@
-# from flask import Flask, render_template, url_for, request, flash, session, redirect
-#
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hfg6h7f'
-#
-# menu = [{"name": "Установка", "url": "install-flask"},
-#         {"name": "Первое приложение", "url": "first-app"},
-#         {"name": "Обратная связь", "url": "contact"}]
-#
-#
-# @app.route("/")
-# def index():
-#     print(url_for('index'))
-#     return render_template('index.html', menu=menu)
-#
-#
-# @app.route("/about")
-# def about():
-#     print(url_for('about'))
-#     return render_template('about.html', title="О сайте", menu=menu)
-#
-#
-# @app.route("/contact", methods=["POST", "GET"])
-# def contact():
-#     if request.method == 'POST':
-#         if len(request.form['username']) > 2:
-#             flash('Сообщение отправлено', category='success')
-#         else:
-#             flash('Ошибка отправки', category='error')
-#
-#     return render_template('contact.html', title="О сайте", menu=menu)
-#
-#
-# @app.route("/profile/<username>")
-# def profile(username):
-#     return f"Профиль пользователя: {username}"
-#
-#
-# @app.route("/login", methods=["POST", "GET"])
-# def login():
-#     if 'userLoggen' in session:
-#         return redirect(url_for('profile', username=session['userLoggen']))
-#     elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
-#         session['userLoggen'] = request.form['username']
-#         return redirect(url_for('profile', username=session['userLoggen']))
-#
-#     return render_template('login.html', title="Авторозация", menu=menu)
-#
-#
-# @app.errorhandler(404)
-# def pagenotfound(error):
-#     return render_template('page404.html', title="Страница не найдена", menu=menu)
-#
-#
-# if __name__ == "__main__":
-#     app.run(debug=True)
 import sqlite3
 import os
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, flash, abort
+from FDataBase import FDataBase
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
@@ -83,21 +28,51 @@ def create_db():
 
 
 def get_db():
-    if not hasattr(g, 'lin_db'):
+    if not hasattr(g, 'link_db'):
         g.link_db = connect_db()
     return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    # '''Закрываем соединение с БД, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
 
 @app.route("/")
 def index():
     db = get_db()
-    return render_template('index.html', menu=[])
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
 
 
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'link_db'):
-        g.link_db.close()
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
+
+
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 
 if __name__ == "__main__":
